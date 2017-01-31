b2b2795a59583c2e7e1ae66d58bf79d62886d96c
beazlr02 <ross.beazley@bbc.co.uk>
2017-01-31 12:52:59 +0000

    Rename test names to be honest

--- pi/src/test/java/uk/co/rossbeazley/centralheating/ui/DisplayTheDefaultScreen.java
+++ pi/src/test/java/uk/co/rossbeazley/centralheating/ui/DisplayTheDefaultScreen.java
@@ -10,7 +10,7 @@ public class DisplayTheDefaultScreen {
 
 
     @Test
-    public void openMenuFromScheduleScreen() {
+    public void theScheduleView() {
 
         CapturingViewFramework capturingViewFramework = new CapturingViewFramework();
 
