b1ffa291c5d8f39c667efeb47fc335c71c9d84aa
rossbeazley <ross@rossbeazley.co.uk>
2017-02-11 22:19:52 +0000

    Extract a couple of controller classes from the presentation tier

---
+++ pi/src/test/java/uk/co/rossbeazley/centralheating/ui/ButtonPressCommand.java
@@ -0,0 +1,5 @@
+package uk.co.rossbeazley.centralheating.ui;
+
+public interface ButtonPressCommand {
+    void buttonPress();
+}
--- pi/src/test/java/uk/co/rossbeazley/centralheating/ui/PresentationTier.java
+++ pi/src/test/java/uk/co/rossbeazley/centralheating/ui/PresentationTier.java
@@ -1,66 +1,83 @@
 package uk.co.rossbeazley.centralheating.ui;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.function.Consumer;
 
 /**
  * Created by beazlr02 on 23/01/17.
  */
 class PresentationTier {
     private final ViewFramework viewFramework;
+    private ButtonPressCommand buttonPressCommand;
     private Model model;
-    private boolean inMenuView = false;
-    private String selectedOption;
-    private int selectedIndex;
-    private List<String> optionsAsString;
-    private int closeIndex;
 
     public PresentationTier(ViewFramework viewFramework, Model model) {
         this.viewFramework = viewFramework;
         this.model = model;
-
         presentScheduleView();
     }
 
     public void buttonPress() {
-        if (inMenuView) {
-            if (selectedIndex == closeIndex) {
-                presentScheduleView();
-            } else {
-                presentConfigurationDialog();
-            }
-        } else {
-            presentMenuView();
-        }
+        buttonPressCommand.buttonPress();
     }
 
     private void presentScheduleView() {
         this.viewFramework.create(ScheduleView.class);
-        inMenuView = false;
+        buttonPressCommand = new ScheduleController(this);
     }
 
     private void presentConfigurationDialog() {
         this.viewFramework.create(ConfigurationDialogView.class);
-        inMenuView = false;
     }
 
     private void presentMenuView() {
-        inMenuView = true;
         MenuView menuView = this.viewFramework.create(MenuView.class);
-        optionsAsString = new ArrayList<>();
-        model.options().forEach(option -> optionsAsString.add(option.name()));
+        buttonPressCommand = new MenuController(this,menuView,model);
+    }
 
-        optionsAsString.add("Close");
 
-        this.closeIndex = optionsAsString.indexOf("Close");
-        menuView.presentOptions(optionsAsString.toArray(new String[optionsAsString.size()]));
 
-        selectedIndex = 0;
+    private static class MenuController implements ButtonPressCommand {
+        private final PresentationTier presentationTier;
+        private Model model;
+        private int selectedIndex;
+        private List<String> optionsAsString;
+        private int closeIndex;
 
-        this.selectedOption = optionsAsString.get(selectedIndex);
 
-        menuView.selectOption(selectedIndex);
+        public MenuController(PresentationTier presentationTier, MenuView menuView, Model model) {
+
+            this.presentationTier = presentationTier;
+            this.model = model;
+            optionsAsString = new ArrayList<>();
+            this.model.options().forEach(option -> optionsAsString.add(option.name()));
+            optionsAsString.add("Close");
+            this.closeIndex = optionsAsString.indexOf("Close");
+            menuView.presentOptions(optionsAsString.toArray(new String[optionsAsString.size()]));
+            selectedIndex = 0;
+            menuView.selectOption(selectedIndex);
+        }
+
+        @Override
+        public void buttonPress() {
+            if (selectedIndex == closeIndex) {
+                this.presentationTier.presentScheduleView();
+            } else {
+                this.presentationTier.presentConfigurationDialog();
+            }
+        }
+    }
+
+    private static class ScheduleController implements ButtonPressCommand {
+        private final PresentationTier presentationTier;
+
+        public ScheduleController(PresentationTier presentationTier) {
+            this.presentationTier = presentationTier;
+        }
+
+        @Override
+        public void buttonPress() {
+            this.presentationTier.presentMenuView();
+        }
     }
 }
