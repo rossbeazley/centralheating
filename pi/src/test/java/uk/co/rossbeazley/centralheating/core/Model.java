ecb3de5e0a895412e701566d2118d0aa364a80a8
rossbeazley <ross@rossbeazley.co.uk>
2017-02-12 21:05:11 +0000

    Removed primitive obsession from Model constructor

--- pi/src/test/java/uk/co/rossbeazley/centralheating/core/Model.java
+++ pi/src/test/java/uk/co/rossbeazley/centralheating/core/Model.java
@@ -1,16 +1,12 @@
 package uk.co.rossbeazley.centralheating.core;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Model {
     private List<Option> options;
 
-    public Model(String... option1) {
-        options = new ArrayList<>(option1.length);
-        for (String option : option1) {
-            options.add(new Option(option));
-        }
+    public Model(List<Option> options) {
+        this.options = options;
     }
 
 
--- pi/src/test/java/uk/co/rossbeazley/centralheating/ui/TestDataBuilder.java
+++ pi/src/test/java/uk/co/rossbeazley/centralheating/ui/TestDataBuilder.java
@@ -1,10 +1,18 @@
 package uk.co.rossbeazley.centralheating.ui;
 
 import uk.co.rossbeazley.centralheating.core.Model;
+import uk.co.rossbeazley.centralheating.core.Option;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestDataBuilder {
     public static Model buildCoreModelWithConfigOptions(String... option1) {
-        return new Model(option1);
+        List<Option> options = new ArrayList<>(option1.length);
+        for (String option : option1) {
+            options.add(new Option(option));
+        }
+        return new Model(options);
     }
 
     public static Model buildAnyCoreModel() {
