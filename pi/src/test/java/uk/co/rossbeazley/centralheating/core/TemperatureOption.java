a05b0fda166df92c9e29d354c5d091dac76fac6a
beazlr02 <ross.beazley@bbc.co.uk>
2017-02-23 12:51:02 +0000

    Delete Temperature Range, never going to be needed

--- pi/src/test/java/uk/co/rossbeazley/centralheating/core/TemperatureOption.java
+++
@@ -1,8 +0,0 @@
-package uk.co.rossbeazley.centralheating.core;
-
-public class TemperatureOption  {
-
-    public static FakeOption createTemperatureOption(String temperatureTitle) {
-        return new FakeOption(temperatureTitle,false);
-    }
-}
