be5b0137adf42c8e77dc7a88fc5e94b79595cf17
beazlr02 <ross.beazley@bbc.co.uk>
2017-03-08 09:51:00 +0000

    Seperate out test tree into specs, fakes, and almost prod

--- pi/src/test/java/uk/co/rossbeazley/centralheating/core/HeatingBoostOption.java
+++
@@ -1,9 +0,0 @@
-package uk.co.rossbeazley.centralheating.core;
-
-public class HeatingBoostOption  {
-
-
-    public static FakeOption createHeatingBoostOption(String option1) {
-        return new FakeOption(option1,false);
-    }
-}
