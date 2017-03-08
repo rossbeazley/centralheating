6e1ef75a72a50d3eb663717a9b8c6092c892ff4b
beazlr02 <ross.beazley@bbc.co.uk>
2017-03-08 20:04:15 +0000

    Choosing ON can turn the gas on

--- pi/src/test/java-specs/uk/co/rossbeazley/centralheating/core/ModelTest.java
+++ pi/src/test/java-specs/uk/co/rossbeazley/centralheating/core/ModelTest.java
@@ -1,9 +1,8 @@
 package uk.co.rossbeazley.centralheating.core;
 
-import org.junit.Ignore;
+import org.fluttercode.datafactory.impl.DataFactory;
 import org.junit.Test;
 
-import java.util.Collections;
 import java.util.List;
 
 import static java.util.Collections.singletonList;
@@ -13,35 +12,79 @@ import static org.junit.Assert.*;
 
 public class ModelTest {
 
-    private static final Object ON = "ON";
-
     @Test
     public void
     haveTheOptionToTurnTheHeatingOn() throws Exception {
-        Model model = new CentralHeatingSystem();
+
+        DataFactory dataFactory = new DataFactory();
+        String onOptionTitle = dataFactory.getRandomWord();
+
+        Model model = buildCentralHeatingSystemWithONOption(onOptionTitle, null);
         List<Option> options = model.options();
-        assertThat(options, hasItem(OptionMatcher.withTitle("on")));
+        assertThat(options, hasItem(OptionMatcher.withTitle(onOptionTitle)));
+    }
+
+    public CentralHeatingSystem buildCentralHeatingSystemWithONOption(String onOptionTitle, GasBurner gasBurner) {
+        return new CentralHeatingSystem(onOptionTitle,gasBurner);
     }
 
     @Test
-    @Ignore("wip")
     public void
     turnsTheHeatingOn() throws Exception {
 
+        GasBurner gasBurner = new GasBurner();
+        Model model = buildCentralHeatingSystemWithONOption("on",gasBurner);
+        List<Option> options = model.options();
+        model.configure(options.get(0), new Model.Callback() {
+            @Override
+            public void OK() {
 
-        Object heating = null;
-        assertThat(heating, is(ON));
+            }
+
+            @Override
+            public void RANGE(HeatingTimeRange heatingTimeRange) {
+
+            }
+        });
+        Object heating = gasBurner.state();
+        assertThat(heating, is(GasBurner.ON));
     }
 
     private static class CentralHeatingSystem implements Model {
+
+        private final Option onOption;
+        private GasBurner gasBurner;
+
+        public CentralHeatingSystem(String onOptionTitle, GasBurner gasBurner) {
+            this.gasBurner = gasBurner;
+            onOption = new Option(onOptionTitle);
+        }
+
         @Override
         public List<Option> options() {
-            return singletonList(new Option("on"));
+            return singletonList(onOption);
         }
 
         @Override
         public void configure(Option option, Callback callback) {
-
+            gasBurner.turnOn();
         }
     }
+
+    private static class GasBurner {
+
+        private static final Object OFF = "GAS OFF",
+                                    ON = "GAS ON";
+
+        private static Object state = OFF;
+
+        public Object state() {
+            return state;
+        }
+
+        public void turnOn() {
+            state = ON;
+        }
+    }
+
 }
