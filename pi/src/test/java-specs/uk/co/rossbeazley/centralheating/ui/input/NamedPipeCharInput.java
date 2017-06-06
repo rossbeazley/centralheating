6dda9ced2236cee10c4349e398f0772e385fc519
rossbeazley <ross@rossbeazley.co.uk>
2017-06-06 21:45:05 +0100

    Add really bad first test

--- pi/src/test/java-specs/uk/co/rossbeazley/centralheating/ui/input/NamedPipeCharInput.java
+++ pi/src/test/java-specs/uk/co/rossbeazley/centralheating/ui/input/NamedPipeCharInput.java
@@ -1,9 +1,9 @@
 package uk.co.rossbeazley.centralheating.ui.input;
 
+import org.junit.Test;
 import uk.co.rossbeazley.centralheating.ui.CanReceiveKeyInput;
 
-import java.io.File;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.UUID;
 
 import static org.hamcrest.core.Is.is;
@@ -13,26 +13,51 @@ public class NamedPipeCharInput {
     private CollectingCanReceiveKeyInput canReceiveKeyInput = new CollectingCanReceiveKeyInput();
 
 
-
+    @Test
     public void
-    readsRight() throws Exception {
+    clockwise() throws Exception {
 
         String pathToPipe = generateNameOfPipeForWriting();
+        createPipe(pathToPipe);
+        new NamedPipeKeyInput(pathToPipe, canReceiveKeyInput);
         FileWriter pipe = openPipe(pathToPipe);
-        new NamedPipeKeyInput(pathToPipe);
 
+        System.out.println("writing");
         pipe.write("c");
+
+        pipe.close();
+
+        Thread.sleep(1000);
 
         assertThat(canReceiveKeyInput.command, is(CollectingCanReceiveKeyInput.Clockwise));
 
     }
 
     private FileWriter openPipe(String pathToPipe) {
-        return null;
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(pathToPipe, true);
+            System.out.println("Got filewriter");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileWriter;
+    }
+
+    private void createPipe(String pathToPipe) {
+        try {
+            System.out.println("Making " + pathToPipe);
+            ProcessBuilder processBuilder = new ProcessBuilder("/usr/bin/mkfifo", pathToPipe);
+            processBuilder.start();
+            System.out.println("Started");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private String generateNameOfPipeForWriting() {
-        return System.getenv("java.io.tmpdir")+File.pathSeparator+UUID.randomUUID().toString();
+        return "/tmp" + File.separator + UUID.randomUUID().toString();
     }
 
     private static class CollectingCanReceiveKeyInput implements CanReceiveKeyInput {
@@ -40,6 +65,7 @@ public class NamedPipeCharInput {
         private static final String ButtonPress = "button press";
         private static final String None = "none";
         private String command = None;
+
         @Override
         public void buttonPress() {
             command = ButtonPress;
@@ -56,7 +82,37 @@ public class NamedPipeCharInput {
     }
 
     private class NamedPipeKeyInput {
-        public NamedPipeKeyInput(String pathToPipe) {
+        public NamedPipeKeyInput(String pathToPipe, CollectingCanReceiveKeyInput canReceiveKeyInput) {
+
+            final FileReader[] fileReader = {null};
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Making filereader");
+                    try {
+                        fileReader[0] = new FileReader(pathToPipe);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Got filereader");
+                    final FileReader finalFileReader = fileReader[0];
+
+                    char c='a';
+                    do {
+                        try {
+                            c = (char) finalFileReader.read();
+                            if (c == 'c') {
+                                canReceiveKeyInput.clockWise();
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    } while (c != 'q');
+
+                }
+            }).start();
+
         }
     }
 }
