646a183166e670d819b36accaed7ec8b55cface7
rossbeazley <ross@rossbeazley.co.uk>
2017-08-10 16:37:48 +0100

    Add kill of python script before exit

--- main/heating.sh
+++ main/heating.sh
@@ -9,11 +9,14 @@ echo ky040
 
 KY_PID=$!
 
-trap ctrl_c INT
+trap killKY040 INT
 
-function ctrl_c() {
-kill $KY_PID
+function killKY040() {
+kill -9 $KY_PID
 }
 
 echo main $!
 bin/main /tmp/pipe
+
+killKY040
+
