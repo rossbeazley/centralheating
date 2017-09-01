d22fcb17cc031eefdcdde536aa040ca086ee3e2d
rossbeazley <ross@rossbeazley.co.uk>
2017-09-01 19:27:44 +0100

    Remove dead code

--- pi/src/main/java/uk/co/rossbeazley/centralheating/ui/input/NamedPipeKeyInputSpike.java
+++ pi/src/main/java/uk/co/rossbeazley/centralheating/ui/input/NamedPipeKeyInputSpike.java
@@ -8,29 +8,21 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.function.Function;
 
-public class NamedPipeKeyInputSpike implements ExternalTimer {
+import static uk.co.rossbeazley.centralheating.ui.input.NamedPipeInputChannel.*;
 
-    private List<Observer> observers = new ArrayList<>();
-
-    private NamedPipeInputChannel.ParseFunction parseFunction;
+public class NamedPipeKeyInputSpike {
 
     public NamedPipeKeyInputSpike(final String pathToPipe, final CanReceiveKeyInput canReceiveKeyInput) {
 
-        HashMap<Character, Runnable> parseFunctions = new HashMap<>();
+        HashMap<Character, Runnable> parseFunctions = new HashMap<Character, Runnable>() {{
+                put('c', canReceiveKeyInput::clockWise);
+                put('a', canReceiveKeyInput::antiClockWise);
+                put('b', canReceiveKeyInput::buttonPress);
+            }};
 
-        parseFunctions.put('c', canReceiveKeyInput::clockWise);
-        parseFunctions.put('a', canReceiveKeyInput::antiClockWise);
-        parseFunctions.put('b', canReceiveKeyInput::buttonPress);
-
-        parseFunction = c -> parseFunctions.getOrDefault(c,()->{}).run();
+        ParseFunction parseFunction = c -> parseFunctions.getOrDefault(c, () -> {}).run();
 
         new NamedPipeInputChannel(pathToPipe, parseFunction);
-
-    }
-
-    @Override
-    public void addObserver(Observer observer) {
-        this.observers.add(observer);
     }
 
 
